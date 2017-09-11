3cd718050d729c5b7e9b0851d0c1e5d6a21a2821
peter <peter@xulukun.cn>
2017-09-11 14:16:53 +0800

    优化gulp执行时传入参数

--- build/util/args.js
+++ build/util/args.js
@@ -2,23 +2,24 @@ import yargs from 'yargs'
 import config from '../../config'
 
 const args = yargs.options({
-  // 'watch': {
-  //   alias: 'w',
-  //   type: 'boolean',
-  //   default: false,
-  //   desc: 'theme changes'
-  // },
   'theme': {
     alias: 't',
     type: 'string',
     desc: 'themes',
-    choices: [...Object.keys(config.themes)]
+    choices: [...Object.keys(config.themes)],
+    demandOption: true
   },
   'mode': {
     alias: 'prod',
     type: 'boolean',
     default: false,
     desc: 'Production mode'
+  },
+  'prot': {
+    alias: 'p',
+    type: 'number',
+    default: 9000,
+    desc: 'Prot'
   }
 })
   .help()
