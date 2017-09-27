c519ec8d27375595853658e28e4027b43935006a
peter <peter@xulukun.cn>
2017-09-27 15:23:24 +0800

    增加api路由代理配置项

--- config/themes.js
+++ config/themes.js
@@ -5,7 +5,8 @@
  *   src: 'Default/default', // 主题放置路径
  *   extraModulesList: [], // 额外生成的模块
  *   includeJsLib: [], // 引入额外的JS库
- *   excludeShallow: [] // 浅移除，不合并到模块中，作为一个http加载
+ *   excludeShallow: [], // 浅移除，不合并到模块中，作为一个http加载
+ *   proxyTable: {} // 设置api路由代理
  * }
  * @type {{point: {name: string, src: string, extraModulesList: Array, includeJsLib: [string,string], excludeShallow: [string]}}}
  */
@@ -15,7 +16,12 @@ const themes = {
   //   src: 'Pointline/point',
   //   extraModulesList: [],
   //   includeJsLib: ['html5shiv.min', 'respond.min'],
-  //   excludeShallow: ['vue', 'modernizr.min']
+  //   excludeShallow: ['vue', 'modernizr.min'],
+  //   proxyTable: {
+  //     '/api/header': {
+  //       target: 'http://infin2.mez100.cn:8035/rest/V1/xmapi/header'
+  //     }
+  //   }
   // }
 }
 
