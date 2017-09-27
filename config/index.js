b6d0238e5b6ba00ee6404fc3560c9b8f2a4aa639
peter <peter@xulukun.cn>
2017-09-27 15:21:05 +0800

    优化网站基本配置项

--- config/index.js
+++ config/index.js
@@ -3,12 +3,12 @@ import themes from './themes'
 /**
  * 网站配置
  * proxy 需要代理网站的URL
- * browser 指定从哪个浏览器打开网站
+ * browser 指定从哪个浏览器打开网站,默认chrome
  * @type {{proxy: string, browser: [string,string]}}
  */
 const siteConfig = {
   proxy: '',
-  browser: ["google chrome", "firefox"]
+  browser: ["google chrome"]
 }
 
 export default {
