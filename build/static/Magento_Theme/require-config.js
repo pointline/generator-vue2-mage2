7a428b19b143fb060d1355c825c5fad1440d170e
peter <peter@xulukun.cn>
2017-11-07 18:38:02 +0800

    修复BUG

--- build/static/Magento_Theme/require-config.js
+++ build/static/Magento_Theme/require-config.js
@@ -10,8 +10,8 @@ const config = {
     'vue': 'Magento_Theme/web/lib/vue',
     'util': 'Magento_Theme/web/util/util',
     'homeApp': 'Magento_Theme/web/components/homeApp',
-    'headerWrapper': 'Magento_Theme/web/components/common/headerWrapper',
-    'footerWrapper': 'Magento_Theme/web/components/common/footerWrapper',
+    'headerWrapper': 'Magento_Theme/web/components/commons/headerWrapper',
+    'footerWrapper': 'Magento_Theme/web/components/commons/footerWrapper',
     'mainContainer': 'Magento_Theme/web/components/mainContainer'
   },
   shim: {
